f6f663774e014a40f4c46a47a6e96bd75d0df964
xntrik <xntrik@gmail.com>
2024-03-09 18:36:20 +0800

    bumped to 0.2.1

--- Formula/threatcl.rb
+++ Formula/threatcl.rb
@@ -1,8 +1,8 @@
 class Threatcl < Formula
   desc "Documenting your Threat Models with HCL"
   homepage "https://github.com/threatcl/threatcl"
-  url "https://github.com/threatcl/threatcl/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "16f19b6b4d744bbf994b7a6c5fc9c286274315383261d7119e88c3866afd8256"
+  url "https://github.com/threatcl/threatcl/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "c7a7ad40fd9dacb1e229696386d46bc0fdea1e621a3dd812f074c64053d7c433"
   license "MIT"
   head "https://github.com/threatcl/threatcl.git", branch: "main"
 
