fdbe5f241718b7ff3557e23f1ddddc1c5e6f77b3
Christian Frichot <678260+xntrik@users.noreply.github.com>
2025-11-19 12:45:11 +0000

    threatcl: update 0.2.8 bottle.

--- Formula/threatcl.rb
+++ Formula/threatcl.rb
@@ -7,8 +7,8 @@ class Threatcl < Formula
   head "https://github.com/threatcl/threatcl.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/threatcl/homebrew-repo/releases/download/threatcl-0.2.7"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e0336b66aacefa2896501e2079b976a31ee3f3e19393568cdc10f600bb970ddb"
+    root_url "https://github.com/threatcl/homebrew-repo/releases/download/threatcl-0.2.8"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ed0b360a21a5352721102afd588141b1934a473aafd1acdcaee14c6df9be461a"
   end
 
   depends_on "go"
